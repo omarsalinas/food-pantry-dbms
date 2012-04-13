0686ace661e703f276aa61eb57800e44e138e906
jonathanpbuck <jonathanpbuck@3c787acd-7e3c-953d-6b00-4aeca1d54d88>
2012-04-13 13:47:08 +0000

    renamed to conform to naming standard

---
+++ src/FamilyTableModel.java
@@ -0,0 +1,32 @@
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * The table model to represent the family information to be displayed when 
+ * adding, editing, or searching a family
+ */
+public class FamilyTableModel extends AbstractTableModel {
+
+	/**
+	 * Default serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
