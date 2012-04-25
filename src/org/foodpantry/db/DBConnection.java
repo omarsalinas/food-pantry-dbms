1aec0c1287b75309a827453d9f50c3a97eaa58ac
marbaugh <marbaugh@gmail.com>
2012-04-25 06:24:59 +0000

    Fixed a few typos and added triton.towson.edu:3306 to the connection URL

--- src/org/foodpantry/db/DBConnection.java
+++ src/org/foodpantry/db/DBConnection.java
@@ -7,14 +7,14 @@ public class DBConnection {
 
 	public static void main(String[] args) {
 		Connection conn = null;
-		String url = "jdbc:mysql://localhost:3306/";
-		String dbName = "test";
+		String url = "jdbc:mysql://triton.towson.edu:3306/marbau1/";
+		String dbName = "";
 		String driver = "com.mysql.jdbc.Driver";
-		String userName = "test"; 
-		String password = "test";
+		String userName = "marbau1"; 
+		String password = "";
 
 		try {
-			System.out.println("Connection to Triton");
+			System.out.println("Connecting to Triton");
 			Class.forName(driver).newInstance();
 			conn = DriverManager.getConnection(url+dbName,userName,password);
 			System.out.println("Connected to mysql on Triton");
