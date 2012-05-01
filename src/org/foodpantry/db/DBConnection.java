0b7c38aaa69508502215838402321e3f2fe06ea2
marbaugh <marbaugh@3c787acd-7e3c-953d-6b00-4aeca1d54d88>
2012-05-01 05:29:03 +0000

    Added constructor for DBConnection and a closeConnection method to close the connection to the database

--- src/org/foodpantry/db/DBConnection.java
+++ src/org/foodpantry/db/DBConnection.java
@@ -2,9 +2,50 @@ package org.foodpantry.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 
+	public Connection connection = null;
+	
+	/**
+	 * Constructor for DBConnection which create a connection to the database
+	 */
+	public DBConnection(){
+		String url = "jdbc:mysql://mysql1007.ixwebhosting.com:3306/";
+		String dbName = "C242386_foodpantry";
+		String driver = "com.mysql.jdbc.Driver";
+		String userName = "C242386_group"; 
+		String password = "Cosc578";
+
+		try {
+			System.out.println("Connecting to Database");
+			Class.forName(driver).newInstance();
+			connection = DriverManager.getConnection(url+dbName,userName,password);
+			System.out.println("Connected to mysql on Server");
+		} catch (Exception e) {
+			System.err.println("Failed to Connect to Datatbase");
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * closeConnection method used to close the connection to the database
+	 */
+	public void closeConnection(){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			System.err.println("Failed to close connection to Datatbase");
+			e.printStackTrace();
+		}
+	}
+	
+	
+	/**
+	 * Main Method used to locally test a database connection
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Connection conn = null;
 		String url = "jdbc:mysql://mysql1007.ixwebhosting.com:3306/";
