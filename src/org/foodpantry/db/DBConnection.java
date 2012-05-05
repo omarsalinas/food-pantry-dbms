4cdae90b45e35eeba573078a3d984278e43ec2f8
HondoKitsune <hondokitsune@gmail.com>
2012-05-05 04:34:11 +0000

    -edited DBConnection.java and Login.java to allow user authentication through database. -added try catch block to LoginDialog.java per the compiler's request -bob secret is now in the security table.

--- src/org/foodpantry/db/DBConnection.java
+++ src/org/foodpantry/db/DBConnection.java
@@ -3,10 +3,15 @@ package org.foodpantry.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DBConnection {
 
 	public Connection connection = null;
+	public boolean Success = false; //used to check if conn was good
+	//public ResultSet results = null;//var to hold result from query
+	public Object results;
 	
 	/**
 	 * Constructor for DBConnection which create a connection to the database
@@ -17,15 +22,19 @@ public class DBConnection {
 		String driver = "com.mysql.jdbc.Driver";
 		String userName = "C242386_group"; 
 		String password = "Cosc578";
-
+		
+		
 		try {
 			System.out.println("Connecting to Database");
 			Class.forName(driver).newInstance();
 			connection = DriverManager.getConnection(url+dbName,userName,password);
 			System.out.println("Connected to mysql on Server");
+			Success = true;
+			
 		} catch (Exception e) {
 			System.err.println("Failed to Connect to Datatbase");
 			e.printStackTrace();
+			Success = false;
 		}
 	}
 	
@@ -41,10 +50,39 @@ public class DBConnection {
 		}
 	}
 	
+	/**
+	 * Function to execute a statement against db which returns no data
+	 * set. Returns true if update was successful.*/
+	public boolean executeUpdate(String statement)
+	{
+		//*TO DO
+		return true;
+	}
 	
 	/**
-	 * Main Method used to locally test a database connection
-	 * @param args
+	 * Function to execute a statement against db which returns a dataset
+	 * @throws SQLException 
+	 */
+	public void executeStatement(String statement) throws SQLException
+	{
+		
+		//create statement object to send statement to db
+		Statement query =  connection.createStatement();
+		
+		//run query against db
+		query.executeQuery(statement);
+		
+		//get result set
+		results = query.getResultSet();
+		
+		//release resources
+		//query.close();
+		
+	}
+	
+	/**
+	  Main Method used to locally test a database connection
+	  @param args
 	 */
 	public static void main(String[] args) {
 		Connection conn = null;
--- src/org/foodpantry/ui/Login.java
+++ src/org/foodpantry/ui/Login.java
@@ -1,16 +1,71 @@
 package org.foodpantry.ui;
+import java.sql.SQLException;
 
+import org.foodpantry.db.*; //import class to create connection objs
+import java.sql.ResultSet;
 /**
  * Borrowed from:
  * http://www.javaswing.org/jdialog-login-dialog-example.aspx
  */
 
 public class Login {
-    public static boolean authenticate(String username, String password) {
-        // hardcoded username and password
-        if (username.equals("bob") && password.equals("secret")) {
+    public static boolean authenticate(String username, String password) throws SQLException {
+    	
+        String query = "select * from Pantry_Security where User_Name='"+username+"'" +
+        		"and password='"+password+"'";
+        
+        String userNameDB = null;//holds user id returned from query
+        String userPwDB = null;//holds user password returned from query
+        
+    	//create connection to database
+    	DBConnection conn = new DBConnection();
+    	
+    	//proceed only if connection was successful
+    	if(conn.Success)
+    	{
+    		//execute user authentication query
+    		conn.executeStatement(query);
+    	  		
+    		//if there is at least one row returned, get it
+    		if (((ResultSet) conn.results).next())
+    		{
+    			//store credentials from db in local vars
+    			userNameDB = ((ResultSet) conn.results).getString(1);
+    			userPwDB = ((ResultSet) conn.results).getString(2);
+    			
+    			System.out.println(userNameDB);
+    			System.out.println(userPwDB);
+    			
+    			//fail if more than one record for username is found
+    			if(((ResultSet)conn.results).next()){
+    				conn.closeConnection();
+    			return false;}
+
+    			//authenication is sucessfull
+    			else if(username.equals(userNameDB) && password.equals(userPwDB)){
+    				conn.closeConnection();
+    				return true;}
+
+    			//user name or password don't match.
+    			else{
+    				conn.closeConnection();
+    				return false;}			
+    		}
+    		else{
+    			//user password combo not found. Return false
+    			return false;
+    		}
+    		
+    	}
+    	else {//crapout
+    		//need to show that user could not log on due to db error?
+    		return false;
+    	}
+    		
+    	// hardcoded username and password
+      /*  if (username.equals("bob") && password.equals("secret")) {
             return true;
         }
-        return false;
+        return false;*/
     }
 }
