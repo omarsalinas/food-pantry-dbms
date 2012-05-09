37562edc3aec088eebf542ce00d52b8b1deab0b4
marbaugh <marbaugh@3c787acd-7e3c-953d-6b00-4aeca1d54d88>
2012-05-09 04:35:10 +0000

    getting closer with the drag and drop

--- src/org/foodpantry/ui/WaitListTransferHandler.java
+++ src/org/foodpantry/ui/WaitListTransferHandler.java
@@ -2,6 +2,7 @@ package org.foodpantry.ui;
 import java.awt.Cursor;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
+import java.awt.dnd.DragSource;
 
 import javax.activation.ActivationDataFlavor;
 import javax.activation.DataHandler;
@@ -24,59 +25,63 @@ import org.foodpantry.ui.WaitTableModel.Reorderable;
  *
  */
 public class WaitListTransferHandler extends TransferHandler {
-
 	/**
-	 * Generated Serial Version UID
+	 * Handles drag & drop row reordering
 	 */
-	private static final long serialVersionUID = 5632344621735413292L;
+	   private final DataFlavor localObjectFlavor = new ActivationDataFlavor(Integer.class, DataFlavor.javaJVMLocalObjectMimeType, "Integer Row Index");
+	   private JTable           table             = null;
 
-	private final DataFlavor localObjectFlavor = new ActivationDataFlavor(Integer.class, DataFlavor.javaJVMLocalObjectMimeType, "Integer Row Index");
+	   public WaitListTransferHandler(JTable table) {
+	      this.table = table;
+	   }
 
-	private JTable table = null;
+	   @Override
+	   protected Transferable createTransferable(JComponent c) {
+	      assert (c == table);
+	      return new DataHandler(new Integer(table.getSelectedRow()), localObjectFlavor.getMimeType());
+	   }
 
-	public WaitListTransferHandler(JTable table) {
-		this.table = table;
+	   @Override
+	   public boolean canImport(TransferHandler.TransferSupport info) {
+	      boolean b = info.getComponent() == table && info.isDrop() && info.isDataFlavorSupported(localObjectFlavor);
+	      table.setCursor(b ? DragSource.DefaultMoveDrop : DragSource.DefaultMoveNoDrop);
+	      return b;
+	   }
+
+	   @Override
+	   public int getSourceActions(JComponent c) {
+	      return TransferHandler.COPY_OR_MOVE;
+	   }
+
+	   @Override
+	   public boolean importData(TransferHandler.TransferSupport info) {
+	      JTable target = (JTable) info.getComponent();
+	      JTable.DropLocation dl = (JTable.DropLocation) info.getDropLocation();
+	      int index = dl.getRow();
+	      int max = table.getModel().getRowCount();
+	      if (index < 0 || index > max)
+	         index = max;
+	      target.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+	      try {
+	         Integer rowFrom = (Integer) info.getTransferable().getTransferData(localObjectFlavor);
+	         if (rowFrom != -1 && rowFrom != index) {
+	            ((Reorderable)table.getModel()).reorder(rowFrom, index);
+	            if (index > rowFrom)
+	               index--;
+	            target.getSelectionModel().addSelectionInterval(index, index);
+	            return true;
+	         }
+	      } catch (Exception e) {
+	         e.printStackTrace();
+	      }
+	      return false;
+	   }
+
+	   @Override
+	   protected void exportDone(JComponent c, Transferable t, int act) {
+	      if (act == TransferHandler.MOVE) {
+	         table.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+	      }
+	   }
+
 	}
-
-	@Override
-	protected Transferable createTransferable(JComponent c) {
-		return new DataHandler(new Integer(table.getSelectedRow()), localObjectFlavor.getMimeType());
-	}
-
-	@Override
-	public int getSourceActions(JComponent c) {
-		return TransferHandler.COPY_OR_MOVE;
-	}
-
-	@Override
-	public boolean importData(TransferHandler.TransferSupport info) {
-		JTable target = (JTable) info.getComponent();
-		JTable.DropLocation dl = (JTable.DropLocation) info.getDropLocation();
-		int index = dl.getRow();
-		int max = table.getModel().getRowCount();
-		if (index < 0 || index > max)
-			index = max;
-		target.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		try {
-			Integer rowFrom = (Integer) info.getTransferable().getTransferData(localObjectFlavor);
-			if (rowFrom != -1 && rowFrom != index) {
-				((Reorderable)table.getModel()).reorder(rowFrom, index);
-				if (index > rowFrom)
-					index--;
-				target.getSelectionModel().addSelectionInterval(index, index);
-				return true;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
-
-	@Override
-	protected void exportDone(JComponent c, Transferable t, int act) {
-		if (act == TransferHandler.MOVE) {
-			table.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		}
-	}
-
-}
