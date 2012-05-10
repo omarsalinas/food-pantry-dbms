9c9c998bfc9d5364f137cd073364ddd2c9030351
jonathanpbuck <jonathanpbuck@3c787acd-7e3c-953d-6b00-4aeca1d54d88>
2012-05-10 14:32:09 +0000

    WaitTableModel: fixed reordering logic issue

--- trunk/src/org/foodpantry/ui/WaitTableModel.java
+++ trunk/src/org/foodpantry/ui/WaitTableModel.java
@@ -265,8 +265,15 @@ public class WaitTableModel extends AbstractTableModel implements Reorderable {
 	@Override
 	public void reorder(int fromIndex, int toIndex) {
 		//TODO needs more testing, this broke and duplicated a row -JB
-		listData.add(toIndex, listData.get(fromIndex));
-		listData.remove(fromIndex);
+		if (toIndex > fromIndex) {
+			listData.add(toIndex, listData.get(fromIndex));
+			listData.remove(fromIndex);
+		} else {
+			listData.add(toIndex, listData.get(fromIndex));
+			listData.remove(fromIndex+1);
+			
+			
+		}
 		this.fireTableDataChanged();
 	}
 	
